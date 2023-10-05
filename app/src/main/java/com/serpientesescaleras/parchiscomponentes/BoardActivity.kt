1b4ef5d7b0da63192f7351b8cbe5b93aa32aff7c
FelipeOrtegaC <139786453+FelipeOrtegaC@users.noreply.github.com>
2023-10-04 23:49:43 -0500

    activity_board.xml y BoardActivity generados para parchis, se debe realizar la implementacion de una matriz 24 * 24 en total 576 textviews

--- app/src/main/java/com/serpientesescaleras/parchiscomponentes/BoardActivity.kt
+++ app/src/main/java/com/serpientesescaleras/parchiscomponentes/BoardActivity.kt
@@ -1,4 +1,15 @@
 package com.serpientesescaleras.parchiscomponentes
 
-class BoardActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class BoardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
 }
